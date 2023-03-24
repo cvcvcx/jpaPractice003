b0ad124061c60dc86557994cc4a065672482c9ac
cvcvcx9 <cvcvcx9@gmaill.com>
2023-03-24 13:09:44 +0900

    [init] 자바 17 버전 설정

---
+++ src/main/java/com/example/jpaPrectice003/JpaPrectice003Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaPrectice003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPrectice003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPrectice003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaPrectice003/JpaPrectice003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaPrectice003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPrectice003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
